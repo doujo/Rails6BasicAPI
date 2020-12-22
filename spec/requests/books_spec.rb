6ef5e621e39b7fcd2a2e672ad9f866bfc6d923ff
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-12-23 00:32:50 +0800

    creating multiple records in cotrollers

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe 'Books API', type: :request do
   describe 'GET /books' do
     before do
-      FactoryBot.create(:book, title: '1984', author: 'George')
-      FactoryBot.create(:book, title: 'The Time Machine', author: 'Weels')
+      author = FactoryBot.create(:author, first_name: 'Maulana', last_name: 'Haikal', age: 22)
+      FactoryBot.create(:book, title: '1984', author_id: author.id)
+      FactoryBot.create(:book, title: 'The Time Machine', author_id: author.id)
     end
 
     it 'returns all books' do
@@ -21,15 +22,22 @@ describe 'Books API', type: :request do
   describe 'POST /books' do
     it 'create a new book' do
       expect {
-        post '/api/v1/books', params: {book: {title: 'MERN stack', author: 'Unknown'}}
+        post '/api/v1/books', params: {
+          book: {title: 'MERN stack'},
+          author: {first_name: 'Maulana', last_name: 'Haikal', age: '22'}
+        }
       }.to change { Book.count }.from(0).to(1)
 
       expect(response).to have_http_status(:created)
+      expect(Author.count).to eq(1)
     end
   end
 
   describe 'DELETE /books/:id' do
-    let!(:book) { FactoryBot.create(:book, title: '1984', author: 'George') }
+    let!(:book) {
+      author = FactoryBot.create(:author, first_name: 'Maulana', last_name: 'Haikal', age: 22)
+      FactoryBot.create(:book, title: '1984', author_id: author.id)
+    }
     it 'deletes a book' do
       expect {
         delete "/api/v1/books/#{book.id}"
