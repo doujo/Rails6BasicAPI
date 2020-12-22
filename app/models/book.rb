68b01247a4f5dea3eb5f872e9ebccd5372f735c6
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-12-22 21:47:41 +0800

    model associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3}
-  validates :author, presence: true, length: {minimum: 3}
+  #validates :author, presence: true, length: {minimum: 3}
+
+  belongs_to :author
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_133113) do
+ActiveRecord::Schema.define(version: 2020_12_22_133840) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 2020_12_22_133113) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
